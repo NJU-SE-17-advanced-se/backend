d0f52debff79d5a23002fef7f5a18ed1a3743eab
Wen Sun <syl1887415157@126.com>
2020-09-29 14:26:27 +0800

    chore(request): 增加RestTemplate请求超时设置

--- api-gateway/src/main/java/org/njuse17advancedse/apigateway/infra/config/RestTemplateConfig.java
+++ api-gateway/src/main/java/org/njuse17advancedse/apigateway/infra/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package org.njuse17advancedse.apigateway.infra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,15 @@ public class RestTemplateConfig {
 
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    return new RestTemplate(getClientHttpRequestFactory());
+  }
+
+  private SimpleClientHttpRequestFactory getClientHttpRequestFactory() {
+    SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+    // 连接超时 10s
+    clientHttpRequestFactory.setConnectTimeout(10_000);
+    // 读取超时 10s
+    clientHttpRequestFactory.setReadTimeout(10_000);
+    return clientHttpRequestFactory;
   }
 }
