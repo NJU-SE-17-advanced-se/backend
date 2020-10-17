cc657f33239d173c837728f5f8b7faf272d5f70c
Wen Sun <syl1887415157@126.com>
2020-10-17 10:59:30 +0800

    refactor: 调整服务设计

---
+++ task-domain-prediction/src/main/java/org/njuse17advancedse/taskdomainprediction/TaskDomainPredictionApplication.java
@@ -0,0 +1,12 @@
+package org.njuse17advancedse.taskdomainprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDomainPredictionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskDomainPredictionApplication.class, args);
+  }
+}
