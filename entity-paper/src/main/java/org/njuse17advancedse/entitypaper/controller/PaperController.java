2f8a30996e749d4878f59ce4f11f1273763bd8f1
Wen Sun <syl1887415157@126.com>
2020-10-17 22:58:58 +0800

    feat(entity-paper): 论文服务初始接口

---
+++ entity-paper/src/main/java/org/njuse17advancedse/entitypaper/controller/PaperController.java
@@ -0,0 +1,71 @@
+package org.njuse17advancedse.entitypaper.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.njuse17advancedse.entitypaper.dto.IPaper;
+import org.njuse17advancedse.entitypaper.dto.IPaperBasic;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/papers")
+@RestController
+public class PaperController {
+
+  @GetMapping("/{id}")
+  // 根据ID获取论文
+  public IPaper getPaper(@PathVariable String id) {
+    return new IPaper(
+      id,
+      "测试论文" + id,
+      "测试摘要" + id,
+      "测试期刊" + id,
+      "2020",
+      "google.com",
+      new ArrayList<>(),
+      new ArrayList<>(),
+      new ArrayList<>()
+    );
+  }
+
+  @GetMapping("")
+  // 根据其他指标获取论文
+  // 如果都没填，返回全部论文
+  // TODO: 分页
+  public List<String> getPaper(
+    @RequestParam(required = false) String researcher,
+    @RequestParam(required = false) String publication,
+    @RequestParam(required = false) String date
+  ) {
+    return new ArrayList<>();
+  }
+
+  @GetMapping("/basic-info/{id}")
+  // 根据指标获取论文简略信息
+  public IPaperBasic getPaperBasicInfo(@PathVariable String id) {
+    return new IPaperBasic(
+      id,
+      "测试论文" + id,
+      "测试摘要" + id,
+      "测试期刊" + id,
+      "2020",
+      new ArrayList<>()
+    );
+  }
+
+  @GetMapping("/basic-info")
+  // 根据指标获取论文简略信息
+  // 如果都没填，返回全部论文的简略信息
+  // TODO: 分页
+  public List<String> getPaperBasicInfo(
+    @RequestParam(required = false) String researcher,
+    @RequestParam(required = false) String publication,
+    @RequestParam(required = false) String date
+  ) {
+    return new ArrayList<>();
+  }
+
+  @GetMapping("/{id}/domains")
+  // 获取论文所属领域
+  public List<String> getDomain(@PathVariable String id) {
+    return new ArrayList<>();
+  }
+}
---
+++ entity-paper/src/main/java/org/njuse17advancedse/entitypaper/dto/IPaperBasic.java
@@ -0,0 +1,23 @@
+package org.njuse17advancedse.entitypaper.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IPaperBasic {
+  private String id;
+
+  private String title;
+
+  private String abs;
+
+  private String publication; // 发表位置
+
+  private String publicationDate; // 发表日期，形如'2020'（目前暂定为年）
+
+  private List<String> researchers;
+}
