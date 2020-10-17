4018ce440f47c65718007b6b3c170c8f7fca242a
Wen Sun <syl1887415157@126.com>
2020-10-17 23:58:08 +0800

    feat(entity-domain): 领域实体服务初始接口

---
+++ entity-domain/src/main/java/org/njuse17advancedse/entitydomain/dto/IDomainBasic.java
@@ -0,0 +1,14 @@
+package org.njuse17advancedse.entitydomain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IDomainBasic {
+  private String id;
+
+  private String name;
+}
