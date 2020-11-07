65c0a6c3957e6d9caddeeff3b7debeef4d93cd9a
Intrejan <1157982786@qq.com>
2020-11-07 19:07:46 +0800

    fix(task-reviewer-recommendation): 删除多余的服务调用

--- task-reviewer-recommendation/src/main/java/org/njuse17advancedse/taskreviewerrecommendation/service/TaskImpactAnalysisService.java
+++ task-reviewer-recommendation/src/main/java/org/njuse17advancedse/taskreviewerrecommendation/service/TaskImpactAnalysisService.java
@@ -1,10 +1,8 @@
 package org.njuse17advancedse.taskreviewerrecommendation.service;
 
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "task-impact-analysis")
 public interface TaskImpactAnalysisService {
