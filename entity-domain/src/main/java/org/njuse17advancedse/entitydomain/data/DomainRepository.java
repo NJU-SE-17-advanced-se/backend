fdcc5010ee82e9a830de573fbc72beef82878ac2
forsakenspirit <2369351080@qq.com>
2020-11-29 14:34:13 +0800

    perf(entity-domain): 改用jpa

---
+++ entity-domain/src/main/java/org/njuse17advancedse/entitydomain/data/DomainRepository.java
@@ -0,0 +1,91 @@
+package org.njuse17advancedse.entitydomain.data;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.njuse17advancedse.entitydomain.dto.IDomain;
+import org.njuse17advancedse.entitydomain.dto.IDomainBasic;
+import org.njuse17advancedse.entitydomain.dto.IResult;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DomainRepository implements AllRepository {
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  public boolean existsById(String domainId) {
+    String sql = "select id from domain where id=:id";
+    return (
+      entityManager
+        .createQuery(sql, String.class)
+        .setParameter("id", domainId)
+        .getResultList()
+        .size() !=
+      0
+    );
+  }
+
+  public IDomain getDomain(String domainId) {
+    IDomainBasic basic = getDomainBasic(domainId);
+    IDomain res = new IDomain();
+    res.setId(domainId);
+    res.setName(basic.getName());
+    res.setPapers(getPapers(domainId));
+    res.setResearchers(getResearchers(domainId));
+    return res;
+  }
+
+  public IDomainBasic getDomainBasic(String domainId) {
+    IDomainBasic res = new IDomainBasic();
+    String sql = "select name from domain where id=:id";
+    res.setName(
+      entityManager
+        .createQuery(sql, String.class)
+        .setParameter("id", domainId)
+        .getSingleResult()
+    );
+    res.setId(domainId);
+    return res;
+  }
+
+  public List<String> getPapers(String domainId) {
+    String sql = "select pid from paper_domain where did=:domainId";
+    return entityManager
+      .createQuery(sql, String.class)
+      .setParameter("domainId", domainId)
+      .getResultList();
+  }
+
+  public List<String> getResearchers(String domainId) {
+    String sql =
+      "select pr.rid from paper_researcher pr join paper_domain pd on pr.pid=pd.pid where pd.did=:domainId";
+    return entityManager
+      .createQuery(sql, String.class)
+      .setParameter("domainId", domainId)
+      .getResultList();
+  }
+
+  public IResult getDomainsByCond(String keyword, int page) {
+    IResult result = new IResult();
+    String sql = "select id from domain where lower(name) like :keyword";
+    result.setIds(
+      entityManager
+        .createQuery(sql, String.class)
+        .setParameter("keyword", "%" + keyword + "%")
+        .setFirstResult((page - 1) * 10)
+        .setMaxResults(10)
+        .getResultList()
+    );
+    sql = "select count(id) from domain where lower(name) like :keyword";
+    result.setCount(
+      Integer.parseInt(
+        entityManager
+          .createQuery(sql)
+          .setParameter("keyword", "%" + keyword + "%")
+          .getSingleResult()
+          .toString()
+      )
+    );
+    return result;
+  }
+}
---
+++ entity-domain/src/main/java/org/njuse17advancedse/entitydomain/entity/JpaPublication.java
@@ -0,0 +1,32 @@
+package org.njuse17advancedse.entitydomain.entity;
+
+import java.util.List;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "publication")
+public class JpaPublication {
+  @Id
+  private String id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "publication_date")
+  private String publicationDate;
+
+  @Column(name = "impact")
+  private double impact;
+
+  @OneToMany(
+    mappedBy = "publication",
+    cascade = CascadeType.ALL,
+    fetch = FetchType.LAZY
+  )
+  private List<JpaPaper> papers;
+}
